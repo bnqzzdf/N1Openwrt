e85303ba24e6b4c702c2ec830edbf7075cacad76
bnqzzdf <bnqzzdf@gmail.com>
2021-01-19 14:31:16 +0800

    merge sh and customed

--- diy.sh
+++ diy.sh
@@ -6,8 +6,8 @@
 # Blog: https://p3terx.com
 #=================================================
 
-echo '修改机器名称'
-sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+#echo '修改机器名称'
+#sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
@@ -15,12 +15,17 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
+# firewall custom
+#echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
-cat > helloworld/luci-app-ssr-plus/root/etc/ssr/black.list << EOF
+cat > helloworld/luci-app-ssr-plus/root/etc/ssrplus/black.list << EOF
 services.googleapis.cn
 googleapis.cn
+heroku.com
+githubusercontent.com 
 EOF
 popd
 
@@ -28,15 +33,18 @@ popd
 mkdir package/community
 pushd package/community
 
+#echo '网易云音乐'
+#git clone  --depth=1 https://github.com/project-openwrt/luci-app-unblockneteasemusic.git 
+
 # Add luci-app-jd-dailybonus
-git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 
 # Add ServerChan
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
 # Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-rm -rf ../lean/luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+#rm -rf ../lean/luci-theme-argon
 popd
 
 # Mod zzz-default-settings
