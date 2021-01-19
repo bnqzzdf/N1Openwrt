18fae113905260aa207c867ca9856d3ff9094425
bnqzzdf <bnqzzdf@gmail.com>
2021-01-19 14:36:56 +0800

    diy upload

--- diy.sh
+++ diy.sh
@@ -12,7 +12,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-echo '修改时区'
+#echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 # firewall custom
